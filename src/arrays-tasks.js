0630d26d0119c0826d370c753da1f627f9f8fcab
Sergey work <s.kruglov@ttksistema.ru>
2024-01-26 17:15:16 +0300

    feat: compleat 39/10

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -50,8 +50,29 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const result = [];
+  if (arr1.length > arr2.length) {
+    arr1.map((item, i) => {
+      if (arr2[i]) {
+        return result.push(item + arr2[i]);
+      }
+      return result.push(item);
+    });
+  } else if (arr1.length < arr2.length) {
+    arr2.map((item, i) => {
+      if (arr1[i]) {
+        return result.push(item + arr1[i]);
+      }
+      return result.push(item);
+    });
+  } else if (arr1.length === arr2.length) {
+    arr1.map((item, i) => {
+      return result.push(item + arr2[i]);
+    });
+  }
+
+  return result;
 }
 
 /**
